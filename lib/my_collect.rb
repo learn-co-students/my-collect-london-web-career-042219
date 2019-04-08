2fa466dcd78fbc11ed56a805cbb1392aa4621ec0
Aiden leeming <aidenjl193@gmail.com>
2019-04-08 15:28:42 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(collection)
+  iterator = 0 
+  temp_collection = []
+  while iterator < collection.length 
+    temp_collection.push(yield collection[iterator])
+    iterator += 1
+  end
+  temp_collection
+end
 
