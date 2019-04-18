80f643939e1aa28ad9b689732b9dac1cc762f561
github username <github email address>
2019-04-18 16:32:49 +0100

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(members)
+  i = 0
+  empty_array = []
+  while i < members.length
+    yield members[i]
+    i += 1
+  end
+end
 
-
+# if languages.any? do |word|
+# return word == word.upcase
+# end
+# if students.any? do |name|
+# name == name.split(" ").first
+# end
